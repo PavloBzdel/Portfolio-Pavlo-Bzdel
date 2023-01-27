4676953a35b0463c30404bf255845bff47302000
Pavlo Bzdel <pavlobzdel@gmail.com>
2023-01-27 22:44:24 +0200

    fixed routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  {path: '' , component: HomeComponent, pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
   {path: 'projects', component: ProjectsComponent},
   {path: 'skills', component: SkillsComponent},
